6415cfda88927ecac3d6d87f916ba1495dc28537
Nuwan <samanthapushpakumara7777@gmail.com>
2025-12-18 09:34:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Nuwan00000/Nuwan-For-Real.1/blob/main/images/Nuwan%20bot%20image.jpg?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 Nuwan WABot Is Alive Now😍*",
+BOT_OWNER: '94787978426',  // Replace with the owner's phone number
 
 
 
